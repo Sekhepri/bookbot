f33f1e3d427af65413273985a5a7e83b57eab539
sekhepri <jonas@jonas.as>
2025-07-12 14:45:47 +0200

    made main.py accept arguments when run. no argument result in exit and status code 1

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from stats import get_num_words, get_num_char, sort_dict
+import sys
+if len(sys.argv) < 2:
+    print("No path to file provided. Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
@@ -6,7 +11,8 @@ def get_book_text(file_path):
 
 
 def main():
-    book_content = get_book_text("books/frankenstein.txt")
+    book_path = sys.argv[1]
+    book_content = get_book_text(book_path)
     number_of_words = get_num_words(book_content)
     number_of_char = get_num_char(book_content)
     number_of_char = sort_dict(number_of_char)
